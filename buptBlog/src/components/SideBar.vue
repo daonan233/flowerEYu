71669f5a555d1646524a72f8c60c9efe0403846f
daonan <91860300+daonan233@users.noreply.github.com>
2023-09-05 13:41:16 +0800

    fix

---
+++ buptBlog/src/components/SideBar.vue
@@ -0,0 +1,55 @@
+<script lang="ts" setup>
+defineProps({
+    userInfo: {
+        type: Object,
+        // 给它个默认值
+        default: () => { }
+    },
+})
+</script>
+
+<template>
+    <div class="sidebar">
+        <a href="可以跳转到个人主页"><el-avatar class="myAvatar" :size="150" src="src/assets/background.png" /></a>
+        <div class="user-info">
+            <h2> {{ userInfo.name }}</h2>
+            <p>ID:&nbsp;{{ userInfo.id }}</p>
+        </div>
+        
+                    
+    </div>
+</template>
+
+<style>
+/* 头像样式 */
+.myAvatar {
+    position: relative;
+    display: flex;
+    margin: auto;
+    margin-top: 100px;
+}
+
+/* 侧边栏显示样式 */
+.sidebar {
+    position: fixed;
+    /* 固定在页面左侧 */
+    top: 0;
+    left: 0;
+    z-index: 10;
+    /* 以保证在nav之下，在波浪之上 */
+    width: 270px;
+    /* 侧边栏宽度 */
+    height: 100vh;
+    /* 与视口高度相同，确保覆盖整个视口 */
+    background-color: rgb(176, 173, 173);
+    text-align: center;
+    /* color: #fff; */
+}
+
+.user-info {
+    color:aliceblue;
+    font-size: 16px;
+}
+</style>
+
+  
---
+++ buptBlog/src/components/video.vue
@@ -0,0 +1,101 @@
+<template>
+  <div id="app">
+    <div id="aplayer"></div>
+  </div>
+</template>
+
+<script>
+import APlayer from "APlayer"; // 引入音乐插件
+import "/node_modules/APlayer/dist/APlayer.min.css"; // 引入音乐插件的样式
+export default {
+  data() {
+    return {
+      audio: [ // 歌曲列表
+        {
+          name: "Beautiful World", 
+          artist: "宇多田ヒカル", 
+          url: 
+            "https://link.jscdn.cn/1drv/aHR0cHM6Ly8xZHJ2Lm1zL3UvcyFBaEJzbFI3bFlkOTVnVFpmYkgxNUt0VHdZQ2VSP2U9ZG9EVUd0.mp3",
+          cover: "https://link.jscdn.cn/1drv/aHR0cHM6Ly8xZHJ2Lm1zL3UvcyFBaEJzbFI3bFlkOTVnVVo0WVJfQldJbGQ0M3VZP2U9NDUyZXVG.jpg", 
+          lrc: "", 
+          theme: "rgb(239, 239, 239)", 
+        },
+        {
+          name: "雪之泪 雨之音",
+          artist: "东京塔子",
+          url:"https://link.jscdn.cn/1drv/aHR0cHM6Ly8xZHJ2Lm1zL3UvcyFBaEJzbFI3bFlkOTVnVFBlZXVOSEVsVFZrNEcxP2U9RFFtMWoy.mp3",
+          cover: "https://pic.imgdb.cn/item/64f6a21d661c6c8e54937879.png",
+          lrc: "",
+          theme: "rgb(212, 127, 100)",
+        },
+        {
+          name: "アスノヨゾラ哨戒班",
+          artist: "Orangestar / IA",
+          url:
+            "https://link.jscdn.cn/1drv/aHR0cHM6Ly8xZHJ2Lm1zL3UvcyFBaEJzbFI3bFlkOTVnUzlDR3AxX3dNdHFrMDNzP2U9Y3RKQmtL.mp3",
+          cover: "https://link.jscdn.cn/1drv/aHR0cHM6Ly8xZHJ2Lm1zL3UvcyFBaEJzbFI3bFlkOTVnVDIyX25INlZFODhzVHRrP2U9YVR0d2J4.png",
+          lrc: "",
+          theme: "#1300a6",
+        },
+		{
+		  name: "Grand Escape",
+		  artist: "Radwinps、三浦透子",
+		  url:
+		    "https://link.jscdn.cn/1drv/aHR0cHM6Ly8xZHJ2Lm1zL3UvcyFBaEJzbFI3bFlkOTVnVEs2RXR1bTlfZWF0a0x6P2U9aXdsT3k0.mp3",
+		  cover: "https://link.jscdn.cn/1drv/aHR0cHM6Ly8xZHJ2Lm1zL3UvcyFBaEJzbFI3bFlkOTVnVVQ2QjFwMllVZXFPWlRoP2U9TVRZN0tJ.jpg",
+		  lrc: "",
+		  theme: "#73d3ff",
+		},
+		{
+		  name: "夕日坂",
+		  artist: "初音ミク/doriko",
+		  url:
+		    "https://link.jscdn.cn/1drv/aHR0cHM6Ly8xZHJ2Lm1zL3UvcyFBaEJzbFI3bFlkOTVnVFYxcjh4TkhXcEdVbG4xP2U9WUczUHJI.mp3",
+		  cover: "https://link.jscdn.cn/1drv/aHR0cHM6Ly8xZHJ2Lm1zL3UvcyFBaEJzbFI3bFlkOTVnVDVyRFM3QTRUblpkNTEwP2U9aXBMODNt.jpg",
+		  lrc: "",
+		  theme: "#ffc821",
+		},
+      ],
+      info: {
+        fixed: true, // 不开启吸底模式
+        listFolded: true, // 折叠歌曲列表
+        autoplay: true, // 开启自动播放
+        preload: "auto", // 自动预加载歌曲
+        loop: "all", // 播放循环模式、all全部循环 one单曲循环 none只播放一次
+        order: "list", //  播放模式，list列表播放, random随机播放
+      },
+    };
+  },
+  mounted() {
+    // 初始化播放器
+    this.initAudio();
+  },
+  methods: {
+    initAudio() {
+      // 创建一个音乐播放器实例，并挂载到DOM上，同时进行相关配置
+      const ap = new APlayer({
+        container: document.getElementById("aplayer"),
+        audio: this.audio, // 音乐信息
+        ...this.info, // 其他配置信息
+      });
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped>
+/*npm install less -D*/
+#app {
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  bottom: 0px;
+  left:0px;
+  z-index: 0;
+
+  #aplayer {
+    width: 300px; // 定个宽度
+	background-color: #39c5bb;
+ }
+}
+</style>
